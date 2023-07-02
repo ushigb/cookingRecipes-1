9191509474bf1cdc6332e3aaa495926207e79d08
Ivan Chipriyanov <ivanchipriqnov@gmail.com>
2023-07-02 15:49:16 +0300

    Add files via upload
    add style to navbar

--- header.php
+++ header.php
@@ -7,31 +7,9 @@ include 'functions.php';
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
             <img class="logo" src="pics/LOGO/Screenshot_2023-04-25-18-19-34-12_a23b203fd3aafc6dcb84e438dda678b6.jpg" alt=""></a></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Начало</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Рецепти</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Категории
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Закуска</a></li>
-                        <li><a class="dropdown-item" href="#">Обяд</a></li>
-                        <li><a class="dropdown-item" href="#">Вечеря</a></li>
-                        <li><a class="dropdown-item" href="#">Салати</a></li>
-                        <li><a class="dropdown-item" href="#">Десерти</a></li>
-                        <li><a class="dropdown-item" href="#">Веган Меню</a></li>            
-                    </ul>
-                </li>                        
-            </ul>
+           
             <div class="container">  
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -76,6 +54,13 @@ include 'functions.php';
     <?php
 }
 ?>
+   
 <header>
     <h1>Готварски рецепти</h1>
 </header>
+    
+    <?php
+    include 'nav.php';
+
+    
+     
---
+++ nav.php
@@ -0,0 +1,29 @@
+<div class="container"> 
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">  
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php" class="list-group-item list-group-item-actionv">Закуска</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php" class="list-group-item list-group-item-actionv">Обяд</a>
+                </li>     
+                <li class="nav-item">
+                    <a class="nav-link " href="index.php" class="list-group-item list-group-item-action">Вечеря</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php" class="list-group-item list-group-item-actionv">Салати</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php" class="list-group-item list-group-item-actionv">Десерти</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php" class="list-group-item list-group-item-actionv">Веган Меню</a>
+                </li>              
+            </ul>
+        </div>
+    </nav>
+        
+    </div>  
+   
